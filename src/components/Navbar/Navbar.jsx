ca3a766ca23e8b3d144e072671fd74871e30674d
Apocryphon06 <hrithikchandrashekar@gmail.com>
2022-10-23 11:55:55 +0530

    navbar menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./ui/apple.png";
-import { Search, WorkOutline } from "@material-ui/icons";
+import { Menu, Search, WorkOutline } from "@material-ui/icons";
 import styled from "styled-components";
 
 const Icon = styled.div`
@@ -17,7 +17,6 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  align-items: center;
   background-color: rgba(0, 0, 0, 0.8);
   border: none;
 `;
@@ -26,13 +25,17 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  padding: 15px;
   justify-content: space-between;
 `;
 
 const List = styled.div`
   display: flex;
   flex-direction: row;
-  padding: 15px;
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const ListItem = styled.li`
@@ -57,11 +60,11 @@ const Logo = styled.img`
 `;
 
 const Banner = styled.div`
+  padding: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 10vh;
   background: linear-gradient(to right, #1d1160, #800080);
 `;
 
@@ -71,12 +74,33 @@ const BannerText = styled.div`
   width: 64%;
   line-height: 1.5em;
   text-align: center;
+  flex-direction: inline;
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+`;
+
+const MenuIcon = styled.div`
+  @media (min-width: 768px) {
+    display: none;
+  }
 `;
 
 function Navbar() {
   return (
     <Container>
       <Wrapper>
+        <MenuIcon>
+          <Menu
+            style={{
+              width: 20,
+              height: 20,
+              color: "#e1d9d1",
+              marginRight: 20,
+              cursor: "pointer",
+            }}
+          />
+        </MenuIcon>
         <Logo src={logo} alt="logo" />
         <List>
           <ListItem>Store</ListItem>
@@ -97,6 +121,7 @@ function Navbar() {
               height: 20,
               color: "#e1d9d1",
               marginRight: 20,
+              cursor: "pointer",
             }}
           />
         </Icon>
@@ -105,6 +130,7 @@ function Navbar() {
             width: 20,
             height: 20,
             color: "#e1d9d1",
+            cursor: "pointer",
           }}
         />
       </Wrapper>
