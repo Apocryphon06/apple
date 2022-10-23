1b5964b7c449fa5a1eb14ac384bd2077a5aa0e7e
Apocryphon06 <hrithikchandrashekar@gmail.com>
2022-10-23 11:03:55 +0530

    navbar icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import logo from "./ui/apple.png";
 import classes from "./Navbar.module.css";
+import { Search, WorkOutline } from "@material-ui/icons";
 
 function Navbar() {
   return (
     <div className={classes.container}>
-      
-      <ul className={classes.ul}>
-      <img className={classes.logo} src={logo} alt="logo" />
-        <li>Store</li>
-        <li>Mac</li>
-        <li>iPad</li>
-        <li>iPhone</li>
-        <li>Watch</li>
-        <li>Airpods</li>
-        <li>TV & Home</li>
-        <li>Only on Apple</li>
-        <li>Accessories</li>
-        <li>Support</li>
-      </ul>
+      <div className={classes.wrapper}>
+        <img className={classes.logo} src={logo} alt="logo" />
+        <ul className={classes.ul}>
+          <li>Store</li>
+          <li>Mac</li>
+          <li>iPad</li>
+          <li>iPhone</li>
+          <li>Watch</li>
+          <li>Airpods</li>
+          <li>TV & Home</li>
+          <li>Only on Apple</li>
+          <li>Accessories</li>
+          <li>Support</li>
+        </ul>
+        <Search
+          style={{
+            width: 20,
+            height: 20,
+            color: "#e1d9d1",
+            marginRight:20
+          }}
+        />
+        <WorkOutline
+          style={{
+            width: 20,
+            height: 20,
+            color: "#e1d9d1",
+          }}
+        />
+      </div>
     </div>
   );
 }
